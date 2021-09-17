2b867817b626f7276829daad6d7291c0288fc6c5
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-09-17 14:54:03 -0300

    Exercício 2

---
+++ src/Exercicio2_Pessoa.java
@@ -0,0 +1,12 @@
+public class Exercicio2_Pessoa {
+    String nome;
+    int idade;
+    double peso = 0;
+    double altura;
+
+    public Exercicio2_Pessoa (String nome, int idade, double altura){
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,10 +1,24 @@
 public class main {
     public static void main(String[] args) {
 
+        // Instanciando objetos Exercício 1
+
         Exercicio1_Bola bolaDeFutebol = new Exercicio1_Bola(70, "branca", "nike", "rápida");
         Exercicio1_Bola bolaDeBasquete = new Exercicio1_Bola(75, "laranja", "spalding", "média");
 
         System.out.println(bolaDeFutebol.circunferencia);
         System.out.println(bolaDeBasquete.cor);
+
+        // Instanciando objetos Exercício 2
+
+        Exercicio2_Pessoa maria = new Exercicio2_Pessoa("maria", 30, 165);
+        Exercicio2_Pessoa joao = new Exercicio2_Pessoa("joao", 2, 80);
+        joao.peso = 20;
+
+        System.out.println(joao.nome);
+        System.out.println(joao.peso);
+        System.out.println(maria.idade);
+        System.out.println(maria.peso);
+
     }
 }
